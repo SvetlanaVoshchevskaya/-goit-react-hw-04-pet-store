e18741ed2a698a57f4795d00a7d8aef2cc626239
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2019-07-15 20:32:47 +0300

    Updates

---
+++ precache-manifest.0c134b42db0b136d4e85484bfc1d0aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe1b20035cfa9afd567136db851bea9",
+    "url": "/-goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "47ee3d4f272174bf1b93",
+    "url": "/-goit-react-hw-04-pet-store/static/css/main.bf556b2b.chunk.css"
+  },
+  {
+    "revision": "bede0c0c8e36028d7cf9",
+    "url": "/-goit-react-hw-04-pet-store/static/js/2.ed64bdd7.chunk.js"
+  },
+  {
+    "revision": "47ee3d4f272174bf1b93",
+    "url": "/-goit-react-hw-04-pet-store/static/js/main.ec8c247e.chunk.js"
+  },
+  {
+    "revision": "77848fa14d9d85c62a4c",
+    "url": "/-goit-react-hw-04-pet-store/static/js/runtime~main.201440c0.js"
+  }
+]);
